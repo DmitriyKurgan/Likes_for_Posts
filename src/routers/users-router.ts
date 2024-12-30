0615226a8bb78634ee994d1a0ebe00a4f1af23b3
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-12-31 00:28:53 +0200

    validationUserUnique

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,7 +1,7 @@
 import {Router} from "express";
 import {UsersController} from "../controllers/UsersController";
 import {validateBasicAuthorization} from "../middlewares/auth/auth-basic";
-import {validateUserFindByParamId} from "../middlewares/validations/find-by-id/user-validation";
+import {validateUserFindByParamId, validationUserUnique} from "../middlewares/validations/find-by-id/user-validation";
 import {validateUsersRequestsInputParams} from "../middlewares/validations/input/user-input-validation";
 import {validateErrorsMiddleware} from "../middlewares/general-errors-validator";
 import {container} from "../composition-root";
@@ -21,6 +21,8 @@ usersRouter.get(
 usersRouter.post(
     '/',
     validateBasicAuthorization,
+    validationUserUnique("login"),
+    validationUserUnique("email"),
     validateUsersRequestsInputParams,
     validateErrorsMiddleware,
     usersController.createUser.bind(usersController)
