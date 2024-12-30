b715328e85318a2057e055172ed206dd8c47a211
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-12-31 01:52:09 +0200

    Check tests

--- src/controllers/TestingController.ts
+++ src/controllers/TestingController.ts
@@ -5,14 +5,7 @@ import { UsersRepository } from "../infrastructure/repositories/users-repository
 import { CommentsRepository } from "../infrastructure/repositories/comments-repository";
 import { SecurityDevicesRepository } from "../infrastructure/repositories/devices-repository";
 import { inject, injectable } from "inversify";
-import {
-  AttemptsModel,
-  BlogModel,
-  CommentsModel,
-  PostsModel,
-  TokensModel,
-  UsersModel, UsersSessionModel
-} from "../infrastructure/repositories/db";
+import {AttemptsModel, TokensModel} from "../infrastructure/repositories/db";
 
 @injectable()
 export class TestingController {
@@ -24,19 +17,13 @@ export class TestingController {
     @inject(SecurityDevicesRepository) protected devicesRepository: SecurityDevicesRepository,
   ) {}
   async deleteEverything(req: Request, res: Response) {
-    // await this.blogsRepository.deleteAll()
-    // await this.postsRepository.deleteAll()
-    // await this.usersRepository.deleteAll()
-    // await this.commentsRepository.deleteAll()
-    // await this.devicesRepository.deleteAll()
-
-    await BlogModel.deleteMany({})
-    await PostsModel.deleteMany({})
-    await UsersModel.deleteMany({})
-    await CommentsModel.deleteMany({})
+    await this.blogsRepository.deleteAll()
+    await this.postsRepository.deleteAll()
+    await this.usersRepository.deleteAll()
+    await this.commentsRepository.deleteAll()
+    await this.devicesRepository.deleteAll()
     await TokensModel.deleteMany({})
     await AttemptsModel.deleteMany({})
-    await UsersSessionModel.deleteMany({})
     res.sendStatus(204)
   }
 }
